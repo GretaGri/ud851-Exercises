c5dc3409e5bbc93738b75c9d335201db3a6b04d6
GretaGri <greta.klimaite@gmail.com>
2019-04-11 00:57:53 +0300

    Completed lesson 11 exercise 02 - used dataBinding to fill in the layout.

--- Lesson11-Completeing-The-UI/T11.02-Exercise-DataBinding/app/src/main/java/com/example/android/boardingpass/MainActivity.java
+++ Lesson11-Completeing-The-UI/T11.02-Exercise-DataBinding/app/src/main/java/com/example/android/boardingpass/MainActivity.java
@@ -1,27 +1,36 @@
 package com.example.android.boardingpass;
 
 /*
-* Copyright (C) 2016 The Android Open Source Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.example.android.boardingpass.databinding.ActivityMainBinding;
+import com.example.android.boardingpass.utilities.FakeDataUtils;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.concurrent.TimeUnit;
+
 public class MainActivity extends AppCompatActivity {
 
-    //TODO (3) Create a data binding instance called mBinding of type ActivityMainBinding
+    //Done (3) Create a data binding instance called mBinding of type ActivityMainBinding
+    ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,22 +38,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        // TODO (4) Set the Content View using DataBindingUtil to the activity_main layout
+        // Done (4) Set the Content View using DataBindingUtil to the activity_main layout
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        // TODO (5) Load a BoardingPassInfo object with fake data using FakeDataUtils
+        // Done (5) Load a BoardingPassInfo object with fake data using FakeDataUtils
+        BoardingPassInfo boardingPassInfo = FakeDataUtils.generateFakeBoardingPassInfo();
 
-        // TODO (9) Call displayBoardingPassInfo and pass the fake BoardingInfo instance
-
+        // Done (9) Call displayBoardingPassInfo and pass the fake BoardingInfo instance
+        displayBoardingPassInfo(boardingPassInfo);
     }
 
     private void displayBoardingPassInfo(BoardingPassInfo info) {
 
-        // TODO (6) Use mBinding to set the Text in all the textViews using the data in info
+        // Done (6) Use mBinding to set the Text in all the textViews using the data in info
+        mBinding.textViewPassengerName.setText(info.passengerName);
+        mBinding.textViewOriginAirport.setText(info.originCode);
+        mBinding.textViewDestinationAirport.setText(info.destCode);
+        mBinding.textViewFlightCode.setText(info.flightCode);
+        mBinding.textViewTerminal.setText(info.departureTerminal);
+        mBinding.textViewGate.setText(info.departureGate);
+        mBinding.textViewSeat.setText(info.seatNumber);
 
-        // TODO (7) Use a SimpleDateFormat formatter to set the formatted value in time text views
+        // Done (7) Use a SimpleDateFormat formatter to set the formatted value in time text views
+        Timestamp boardingTime = info.boardingTime;
+        Timestamp departureTime = info.departureTime;
+        Timestamp arrivalTime = info.arrivalTime;
+        SimpleDateFormat dateFormaterAM = new SimpleDateFormat("h:mm a");
+        mBinding.textViewBoardingTime.setText(dateFormaterAM.format(boardingTime));
+        mBinding.textViewDepartureTime.setText(dateFormaterAM.format(departureTime));
+        mBinding.textViewArrivalTime.setText(dateFormaterAM.format(arrivalTime));
 
-        // TODO (8) Use TimeUnit methods to format the total minutes until boarding
-
+        // Done (8) Use TimeUnit methods to format the total minutes until boarding
+        Long minutesUntilBoarding = info.getMinutesUntilBoarding();
+        SimpleDateFormat dateFormater = new SimpleDateFormat("h:mm");
+        mBinding.textViewBoardingInCountdown.setText(dateFormater.format(minutesUntilBoarding));
     }
 }
 
